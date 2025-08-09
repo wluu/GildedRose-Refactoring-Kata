e60b232909af11282e89c803b2b0e7ea2385929b
Wilson Luu <wluu@expediagroup.com>
2025-08-09 16:58:59 -0700

    day 4 of kata cont'd: updated the variable name so it makes more intuitive sense

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -4,30 +4,30 @@ class GildedRose(val items: List<Item>) {
     fun updateQuality() {
         items.forEach { item ->
             val itemName = item.name
-            val itemQuality = item.quality
+            val currentQuality = item.quality
 
             if (itemName != FineGoods.AGED_BRIE.rawName && itemName != FineGoods.BACKSTAGE_PASS.rawName) {
-                if (itemQuality > 0) {
+                if (currentQuality > 0) {
                     if (itemName != FineGoods.SULFURAS.rawName) {
-                        item.quality = itemQuality - 1
+                        item.quality = currentQuality - 1
                     }
                 }
             } else {
-                if (itemQuality < MAX_QUALITY_50) {
+                if (currentQuality < MAX_QUALITY_50) {
                     if (itemName == FineGoods.BACKSTAGE_PASS.rawName) {
-                        var updatedPassQuality = itemQuality + 1
+                        var updatedPassQuality = currentQuality + 1
 
                         if (item.sellIn <= SELL_IN_10_DAYS) {
-                            updatedPassQuality = itemQuality + 2
+                            updatedPassQuality = currentQuality + 2
                         }
 
                         if (item.sellIn <= SELL_IN_5_DAYS) {
-                            updatedPassQuality = itemQuality + 3
+                            updatedPassQuality = currentQuality + 3
                         }
 
                         item.quality = updatedPassQuality
                     } else {
-                        item.quality = itemQuality + 1
+                        item.quality = currentQuality + 1
                     }
                 }
             }
@@ -41,7 +41,7 @@ class GildedRose(val items: List<Item>) {
                     if (itemName == FineGoods.BACKSTAGE_PASS.rawName) {
                         item.quality = 0
                     } else {
-                        if (itemQuality > 0) {
+                        if (currentQuality > 0) {
                             if (itemName != FineGoods.SULFURAS.rawName) {
                                 // can't replace item.quality with itemQuality val; breaks unit tests
                                 item.quality = item.quality - 1
@@ -49,7 +49,7 @@ class GildedRose(val items: List<Item>) {
                         }
                     }
                 } else {
-                    if (itemQuality < MAX_QUALITY_50) {
+                    if (currentQuality < MAX_QUALITY_50) {
                         // can't replace item.quality with itemQuality val; breaks unit tests
                         item.quality = item.quality + 1
                     }
