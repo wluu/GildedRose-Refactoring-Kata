230276da8444c20445900e146a61e1e92c02560a
Wilson Luu <wluu@expediagroup.com>
2025-08-09 17:07:34 -0700

    day 4 of kata cont'd: updated the helper function to be more descriptive

--- Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -6,8 +6,8 @@ import org.junit.jupiter.api.Test
 internal class GildedRoseTest {
     @Test
     fun `item name should not change after calling updateQuality`() {
-        val item = updateQuality(
-            passingDays = 10,
+        val item = endOfDayUpdatedQuality(
+            daysPassed = 10,
             Item(name = "regular item", sellIn = 1, quality = 0)
         )
 
@@ -16,8 +16,8 @@ internal class GildedRoseTest {
 
     @Test
     fun `0 day passed for regular item, sellIn and quality did not change`() {
-        val item = updateQuality(
-            passingDays = 0,
+        val item = endOfDayUpdatedQuality(
+            daysPassed = 0,
             Item(name = "regular item", sellIn = 1, quality = 0)
         )
 
@@ -27,8 +27,8 @@ internal class GildedRoseTest {
 
     @Test
     fun `1 day passed for regular item, sellIn is -1, quality did not change`() {
-        val item = updateQuality(
-            passingDays = 1,
+        val item = endOfDayUpdatedQuality(
+            daysPassed = 1,
             Item(name = "regular item", sellIn = 0, quality = 0)
         )
 
@@ -38,8 +38,8 @@ internal class GildedRoseTest {
 
     @Test
     fun `quality degrades twice as fast once the regular item sell by date has passed`() {
-        val item = updateQuality(
-            passingDays = 2,
+        val item = endOfDayUpdatedQuality(
+            daysPassed = 2,
             // day 1: quality = 6 - 1; 5
             // day 2: passed sell by date, quality = 5 - 2; 3
             Item(name = "regular item", sellIn = 1, quality = 6)
@@ -51,8 +51,8 @@ internal class GildedRoseTest {
 
     @Test
     fun `Aged Brie increases in Quality the older it gets`() {
-        val item = updateQuality(
-            passingDays = 1,
+        val item = endOfDayUpdatedQuality(
+            daysPassed = 1,
             Item(name = "Aged Brie", sellIn = 2, quality = 1)
         )
 
@@ -62,8 +62,8 @@ internal class GildedRoseTest {
 
     @Test
     fun `Aged Brie increases in Quality twice as fast past the sell by date`() {
-        val item = updateQuality(
-            passingDays = 2,
+        val item = endOfDayUpdatedQuality(
+            daysPassed = 2,
             Item(name = "Aged Brie", sellIn = 1, quality = 1)
         )
 
@@ -73,8 +73,8 @@ internal class GildedRoseTest {
 
     @Test
     fun `Aged Brie Quality cannot be more than 50`() {
-        val item = updateQuality(
-            passingDays = 100,
+        val item = endOfDayUpdatedQuality(
+            daysPassed = 100,
             Item(name = "Aged Brie", sellIn = 1, quality = 1)
         )
 
@@ -84,8 +84,8 @@ internal class GildedRoseTest {
 
     @Test
     fun `Sulfuras quality does not drop and sellIn is the same, sellIn = 1`() {
-        val item = updateQuality(
-            passingDays = 100,
+        val item = endOfDayUpdatedQuality(
+            daysPassed = 100,
             Item(name = "Sulfuras, Hand of Ragnaros", sellIn = 1, quality = 80)
         )
 
@@ -97,8 +97,8 @@ internal class GildedRoseTest {
     fun `Sulfuras quality does not drop and sellIn is the same, sellIn is -1 `() {
         // this is needed to get the false use case on line 42
         // TODO: the sulfuras use cases needed to be refactored to make more sense
-        val item = updateQuality(
-            passingDays = 100,
+        val item = endOfDayUpdatedQuality(
+            daysPassed = 100,
             Item(name = "Sulfuras, Hand of Ragnaros", sellIn = -1, quality = 80)
         )
 
@@ -108,8 +108,8 @@ internal class GildedRoseTest {
 
     @Test
     fun `Backstage passes Quality increase by 1 when there are 10 days left`() {
-        val item = updateQuality(
-            passingDays = 1,
+        val item = endOfDayUpdatedQuality(
+            daysPassed = 1,
             Item(name = "Backstage passes to a TAFKAL80ETC concert", sellIn = 11, quality = 1)
         )
 
@@ -119,8 +119,8 @@ internal class GildedRoseTest {
 
     @Test
     fun `Backstage passes Quality increase by 2 when there less than 10 days left`() {
-        val item = updateQuality(
-            passingDays = 2,
+        val item = endOfDayUpdatedQuality(
+            daysPassed = 2,
             Item(name = "Backstage passes to a TAFKAL80ETC concert", sellIn = 11, quality = 1)
         )
 
@@ -130,8 +130,8 @@ internal class GildedRoseTest {
 
     @Test
     fun `Backstage passes Quality increase by 2 when there are 5 days left`() {
-        val item = updateQuality(
-            passingDays = 6,
+        val item = endOfDayUpdatedQuality(
+            daysPassed = 6,
             Item(name = "Backstage passes to a TAFKAL80ETC concert", sellIn = 11, quality = 1)
         )
 
@@ -141,8 +141,8 @@ internal class GildedRoseTest {
 
     @Test
     fun `Backstage passes Quality increase by 3 when there less than 5 days left`() {
-        val item = updateQuality(
-            passingDays = 7,
+        val item = endOfDayUpdatedQuality(
+            daysPassed = 7,
             Item(name = "Backstage passes to a TAFKAL80ETC concert", sellIn = 11, quality = 1)
         )
 
@@ -152,8 +152,8 @@ internal class GildedRoseTest {
 
     @Test
     fun `Backstage passes Quality drops to 0 after the concert`() {
-        val item = updateQuality(
-            passingDays = 12,
+        val item = endOfDayUpdatedQuality(
+            daysPassed = 12,
             Item(name = "Backstage passes to a TAFKAL80ETC concert", sellIn = 11, quality = 1)
         )
 
@@ -161,10 +161,10 @@ internal class GildedRoseTest {
         assertEquals(0, item.quality)
     }
 
-    private fun updateQuality(passingDays: Int, item: Item): Item {
+    private fun endOfDayUpdatedQuality(daysPassed: Int, item: Item): Item {
         val gildedRose = GildedRose(listOf(item))
 
-        for (i in 0..< passingDays) {
+        for (i in 0..< daysPassed) {
             gildedRose.updateQuality()
         }
 
